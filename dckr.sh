6d59d717df4735f4d922f24d9d8b5a8aea11ee76
SomesH S <sssomeshhh@gmail.com>
2022-11-16 16:41:51 +0530

    docker ps

--- dckr.sh
+++ dckr.sh
@@ -9,5 +9,6 @@ docker build . \
     --build-arg X=$1 \
     --build-arg R=$2;
 docker run --rm -it --name=cntnr-$1d pack-$1d:temp bash;
+docker ps -a;
 docker cp cntnr-$1d:$2/$1t.xz ./;
 ls -hls $1t.xz;
