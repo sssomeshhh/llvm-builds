df9bb2bc2772846d7c7f89b2f3072205d3e2d808
SomesH S <sssomeshhh@gmail.com>
2022-11-16 17:10:33 +0530

    re - 81fbbfe0

--- dckr.sh
+++ dckr.sh
@@ -8,7 +8,13 @@ docker build . \
     --build-arg BASE_IMAGE_PACKAGE=$3 \
     --build-arg X=$1 \
     --build-arg R=$2;
-docker run --rm -it --name=cntnr-$1d pack-$1d:temp bash;
+docker run \
+    --rm \
+    --tty \
+    --detach \
+    --interactive \
+    --name=cntnr-$1d pack-$1d:temp bash;
 docker ps -a;
 docker cp cntnr-$1d:$2/$1t.xz ./;
 ls -hls $1t.xz;
+docker stop cntnr-$1d;
